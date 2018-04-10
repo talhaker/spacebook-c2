2dc330dcf76e09d1c0747c3a1d2cfac52e99479b
talhaker <tal.haker@gmail.com>
2018-04-10 15:04:49 +0300

    Individual Exercise 3

--- main.js
+++ main.js
@@ -13,10 +13,22 @@ let addPost = function(postText) {
 let renderPosts = function() {
     // Empty current content of posts list
     let postList = $('.posts');
-    posts.empty();
+    postList.empty();
     // Display updated beer list
     posts.forEach(function(post) {
-        $('<p class="post" data-id="' + posts.id + '">' + posts.text + '</p>').appendTo(postList);
+        $('<p class="post" data-id="' + post.id + '"> <button type="button" class="remove">REMOVE</button> ' + post.text + '</p>').appendTo(postList);
     });
 
 }
+
+let postHandler = function() {
+    let text = $('.form-control').val();
+    addPost(text);
+    renderPosts();
+}
+
+
+$('.add-post').click(postHandler);
+$('.posts').on('click', '.post', function() {
+    $(this).remove();
+});
